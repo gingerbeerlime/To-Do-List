61a53abbd6c688f015d22ccc466e513a73e2f5f9
Sumin <lsumin1127@gmail.com>
2022-05-30 18:11:13 +0900

    [refact]js refactoring

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const addBtn = document.querySelector('.btn-add')
     const todoList = document.querySelector('.todo-list')
     const clearAllBtn = document.querySelector('.btn-clear-all')
-
-    // todolist 비었을 때 msg
-    const noItemMsg = document.createElement('p')
-    noItemMsg.textContent = 'To Do List가 비어있습니다.'
-    noItemMsg.classList.add('no-item')
+    const emptyListMsg = document.createElement('p')
+    emptyListMsg.textContent = 'To Do List가 비어있습니다.'
+    emptyListMsg.classList.add('empty-todo')
 
     // [class]localStorage value값<object> 생성 클래스
     class Todo {
@@ -17,148 +15,124 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // [func]localStorage 데이터 가져오는 함수 <Array>
-    function getTodoData () {
+    // [function]
+    const getTodoData = () => {
         return JSON.parse(localStorage.getItem('todo-data'))
     }
 
-    // [func]localStorage 데이터 갱신하는 함수
-    // @param newData : <Array>로 전달
-    function setTodoData (newData) {
+    // @param newData <Array>
+    const setTodoData = (newData) => {
         localStorage.setItem('todo-data', JSON.stringify(newData))
     }
 
-    // [func]parentNode 중 <li> 태그 찾아서 반환하는 함수
-    function findLINode (eTarget) {
+    const findParentLINode = (eTarget) => {
         while (eTarget.parentNode.tagName !== 'LI') {
             eTarget = eTarget.parentNode
         }
         return eTarget.parentNode
     }
 
-    // [func]조작하는 todoItem(li태그) index 가져오는 함수
-    function getIndex (eTarget) {
+    const getIndex = (eTarget) => {
         const itemList = document.querySelectorAll('li')
-        const item = eTarget.tagName === 'LI' ? eTarget : findLINode(eTarget)
-
-        for (let idx = 0; idx < itemList.length; idx++) {
-            if (item === itemList[idx]) return idx
-        }
+        const eventTarget = eTarget.tagName === 'LI' ? eTarget : findParentLINode(eTarget)
+        const idx = [...itemList].findIndex(item => item === eventTarget)
+        return idx
     }
 
-    // [func]todoItem(li태그) 생성 함수
     // @param todoObj <object> { txt : todo내용, checked : true/false(default) }
     const createTodoItem = (todoObj) => {
-        const item = document.createElement('li')
-        const wrapper = document.createElement('div')
-        const chkLbl = document.createElement('label')
-        const chkInput = document.createElement('input')
-        const chkIcon = document.createElement('span')
-        const text = document.createElement('span')
+        const todoItem = document.createElement('li')
+        const todoBox = document.createElement('div')
+        const checkboxLabel = document.createElement('label')
+        const checkboxInput = document.createElement('input')
+        const checkboxIcon = document.createElement('span')
+        const todoText = document.createElement('span')
         const removeBtn = document.createElement('button')
         const removeIcon = document.createElement('i')
         const sortIcon = document.createElement('i')
 
-        // checkbox 조작(label, input, span)
-        chkInput.type = 'checkbox'
-        chkInput.classList.add('check-todo')
-        chkIcon.classList.add('check-icon')
-        chkLbl.classList.add('check-label')
-        chkLbl.append(chkInput, chkIcon)
+        // Node 트리 구성
+        checkboxInput.type = 'checkbox'
+        checkboxInput.classList.add('check-todo')
+        checkboxIcon.classList.add('check-icon')
+        checkboxLabel.classList.add('check-label')
+        checkboxLabel.append(checkboxInput, checkboxIcon)
+
         // @param : todoObj.checked
         if (todoObj.checked) {
-            chkInput.checked = true
-            text.className = 'todo-text-done'
+            checkboxInput.checked = true
+            todoText.className = 'todo-text-done'
         }
 
-        // text 조작
         // @param : todoObj.txt
-        text.textContent = todoObj.txt
-        text.classList.add('todo-text')
+        todoText.textContent = todoObj.txt
+        todoText.classList.add('todo-text')
 
-        // removeBtn 조작
         removeIcon.className = 'fa-solid fa-trash'
-        removeBtn.append(removeIcon)
+        sortIcon.className = 'fa-solid fa-sort icon-sort'
         removeBtn.type = 'button'
         removeBtn.classList.add('btn-remove')
+        removeBtn.append(removeIcon)
 
-        // sortIcon 조작
-        sortIcon.className = 'fa-solid fa-sort icon-sort'
+        todoBox.append(checkboxLabel, todoText, removeBtn, sortIcon)
+        todoBox.classList.add('todo-box')
 
-        // wrapper(div태그) 조작
-        wrapper.append(chkLbl, text, removeBtn, sortIcon)
-        wrapper.classList.add('todo-wrap')
+        todoItem.append(todoBox)
+        todoItem.classList.add('todo-item')
 
-        // item(li태그) 조작
-        item.append(wrapper)
-        item.classList.add('todo-item')
-
-        // -[func]todoItem localStorage 삭제 함수
-        const removeTodo = (idx) => {
+        // [function]
+        const removeTodo = (removeIdx) => {
             const todoData = getTodoData()
-            todoData.splice(idx, 1)
-            setTodoData(todoData)
+            const newTodoData = todoData.filter((data, idx) => idx !== removeIdx)
+            setTodoData(newTodoData)
+            todoList.removeChild(todoList.children[removeIdx])
         }
 
-        // -[event]todoItem(li태그) 개별 삭제
-        removeBtn.addEventListener('click', (e) => {
-            const removeItem = findLINode(e.target)
-            const removeIdx = getIndex(e.target)
-            // localStorage 삭제
-            removeTodo(removeIdx)
-            // 노드 삭제
-            todoList.removeChild(removeItem)
-            // todo Count
-            const todoCnt = document.getElementsByTagName('LI').length
-            if (todoCnt === 0) todoList.append(noItemMsg)
-        })
-
-        // -[event]checkbox check 이벤트
-        chkInput.addEventListener('change', (e) => {
-            const idx = getIndex(e.target)
+        const dragDownCheckedTodo = (checkedIdx) => {
             const todoData = getTodoData()
-            // text 스타일 변경
-            text.className = e.target.checked ? 'todo-text-done' : 'todo-text'
-            // 체크 상태 true <-> false 전환
-            todoData[idx].checked = !todoData[idx].checked
-            // if[checked:true] - 리스트 맨 하단으로 이동, else[checked:false] - 첫번째 체크 항목 위로 이동
-            if (todoData[idx].checked) {
-                // Node 이동
-                todoList.append(todoList.removeChild(todoList.children[idx]))
-                // localStorage 값 이동
-                todoData.push(...todoData.splice(idx, 1))
+            todoData[checkedIdx].checked = !todoData[checkedIdx].checked
+            if (todoData[checkedIdx].checked) {
+                todoList.append(todoList.removeChild(todoList.children[checkedIdx]))
+                todoData.push(...todoData.splice(checkedIdx, 1))
             } else {
-                const firstChkIdx = todoData.findIndex(todo => todo.checked === true)
-                if (idx !== (firstChkIdx - 1)) {
-                    // Node 이동
-                    todoList.insertBefore(todoList.removeChild(todoList.children[idx]), todoList.children[firstChkIdx] ?? null)
-                    if (firstChkIdx !== -1) {
-                        // localStorage 값 이동
-                        todoData.splice(firstChkIdx, 0, ...todoData.splice(idx, 1))
+                const firstCheckedIdx = todoData.findIndex(todo => todo.checked === true)
+                if (checkedIdx !== (firstCheckedIdx - 1)) {
+                    todoList.insertBefore(todoList.removeChild(todoList.children[checkedIdx]), todoList.children[firstCheckedIdx] ?? null)
+                    if (firstCheckedIdx !== -1) {
+                        todoData.splice(firstCheckedIdx, 0, ...todoData.splice(checkedIdx, 1))
                     }
                 }
             }
             setTodoData(todoData)
+        }
+
+        // [event]
+        removeBtn.addEventListener('click', (e) => {
+            removeTodo(getIndex(e.target))
+            const todoCount = document.getElementsByTagName('LI').length
+            if (todoCount === 0) todoList.append(emptyListMsg)
         })
 
-        return item
+        checkboxInput.addEventListener('change', (e) => {
+            todoText.className = e.target.checked ? 'todo-text-done' : 'todo-text'
+            dragDownCheckedTodo(getIndex(e.target))
+        })
+
+        return todoItem
     }
 
-    // [func]처음 로딩시 localStorage 데이터 불러오기
     (function loadTodo () {
         localStorage.getItem('todo-data') ?? localStorage.setItem('todo-data', JSON.stringify([]))
         const todoData = getTodoData()
         if (todoData.length === 0) {
-            todoList.append(noItemMsg)
+            todoList.append(emptyListMsg)
         } else {
-            const todoArr = JSON.parse(localStorage.getItem('todo-data'))
-            todoArr.forEach(todoObj => todoList.append(createTodoItem(todoObj)))
+            const todoData = getTodoData()
+            todoData.forEach(todoObj => todoList.append(createTodoItem(todoObj)))
         }
     })()
 
-    // [func]todo 추가 함수
     const addTodo = () => {
-        // 입력값 유효성 검증
         const todoText = inputTodo.value.trim()
         if (!todoText) {
             alert('할 일을 입력해주세요')
@@ -167,26 +141,20 @@ document.addEventListener('DOMContentLoaded', () => {
             return
         }
 
-        // localStorage에 저장할 value값<object> 생성
         const todoObj = new Todo(todoText)
 
-        // localStorage 데이터 저장
         const todoData = getTodoData()
-        // 첫번째 checked === true 요소 앞으로 추가
         let insertIdx = todoData.findIndex(todo => todo.checked === true)
         if (insertIdx === -1) insertIdx = todoList.childElementCount
         todoData.splice(insertIdx, 0, todoObj)
         setTodoData(todoData)
 
-        // todoItem(li태그) 생성 후 추가
         todoList.insertBefore(createTodoItem(todoObj), todoList.children[insertIdx])
 
-        // todoItem 추가 후 입력창 초기화
         inputTodo.value = null
 
-        // todo Count
-        const todoCnt = document.getElementsByTagName('li').length
-        if (todoCnt === 1) todoList.removeChild(noItemMsg)
+        const todoCount = document.getElementsByTagName('li').length
+        if (todoCount === 1) todoList.removeChild(emptyListMsg)
     }
 
     // [event]todo 추가 함수 호출
@@ -202,8 +170,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         // 빈 배열로 초기화
         setTodoData([])
-        todoList.append(noItemMsg)
-        document.getElementById('switch').checked = false
+        todoList.append(emptyListMsg)
+        document.getElementById('sort-switch').checked = false
     })
 
     // ----------------------------------------------------//
@@ -212,10 +180,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const editInput = document.querySelector('.input-edit')
     const updateBtn = document.querySelector('.btn-update')
 
-    // [func]모달 열기
+    // [function]
     const openModal = (e) => {
         editModal.style.display = 'flex'
-        // localStorage txt 값 가져와서 input창에 보여주기
         const idx = getIndex(e.target)
         const targetObj = getTodoData()[idx]
         editInput.value = targetObj.txt
@@ -223,27 +190,22 @@ document.addEventListener('DOMContentLoaded', () => {
         sessionStorage.setItem('edit', idx)
     }
 
-    // [func]모달 닫기
-    const closeModal = () => {
-        editModal.style.display = 'none'
-    }
-
-    // [func]todo 수정 업데이트
     const updateTodo = () => {
-        // 수정 데이터 가져오기
         const editIdx = sessionStorage.getItem('edit')
         const editItem = todoList.children[editIdx]
-        const editTxt = editItem.firstChild.children[1]
+        const editTxt = editItem.querySelector('.todo-text')
         const todoData = getTodoData()
 
-        // localStorage 변경된 값 저장
         todoData[editIdx].txt = editInput.value
         setTodoData(todoData)
 
-        // span태그 text 변경
         editTxt.textContent = editInput.value
 
         closeModal()
+    }
+
+    const closeModal = () => {
+        editModal.style.display = 'none'
     }
 
     todoList.addEventListener('dblclick', openModal)
@@ -252,7 +214,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.keyCode === 13) updateTodo()
     })
     editModal.addEventListener('click', (e) => {
-        if (e.target.className === 'modal-wrap' || e.target.tagName === 'I') closeModal()
+        if (e.target.className === 'modal-wrap') closeModal()
     })
 
     // ----------------------------------------------------//
@@ -260,12 +222,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // [func]drag start : 이동시킬 데이터 저장
     function onDragStart (e) {
-        console.log(e.target)
         e.dataTransfer.setData('idx', getIndex(e.target))
-        const todoWrap = e.currentTarget.firstChild
-        todoWrap.style.backgroundColor = '#588b8d'
-        todoWrap.children[1].style.color = 'white'
-        todoWrap.children[3].style.color = 'white'
+        const todoBox = e.currentTarget.querySelector('.todo-box')
+        todoBox.style.backgroundColor = '#588b8d'
+        todoBox.children[1].style.color = 'white'
+        todoBox.children[3].style.color = 'white'
     }
 
     // [func]drop : 데이터 순서 바꾸기
@@ -297,17 +258,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // [func]drag end : 이벤트 완료시 원래 style로 복귀
     function onDragEnd (e) {
-        const todoWrap = e.currentTarget.firstChild
-        todoWrap.style.backgroundColor = '#c2e0d8de'
-        todoWrap.children[3].style.color = '#588b8d'
-        todoWrap.children[1].style.color = todoWrap.children[0].children[0].checked ? '#8f959b' : '#000'
+        const todoBox = e.currentTarget.querySelector('.todo-box')
+        todoBox.style.backgroundColor = '#c2e0d8de'
+        todoBox.children[3].style.color = '#588b8d'
+        todoBox.children[1].style.color = todoBox.children[0].children[0].checked ? '#8f959b' : '#000'
     }
 
     const todoItems = document.getElementsByTagName('li')
-    const chkSort = document.querySelector('.sort-check')
+    const chkSort = document.getElementById('input-sort')
 
     // [event]수동 정렬 상태 전환(체크박스)
     chkSort.addEventListener('change', (e) => {
+        console.log('eee')
         const sortMode = !!e.target.checked
         const removeBtns = document.querySelectorAll('.btn-remove')
         const sortIcons = document.querySelectorAll('.icon-sort')
@@ -358,7 +320,7 @@ document.addEventListener('DOMContentLoaded', () => {
             while (todoList.hasChildNodes()) {
                 todoList.removeChild(todoList.firstChild)
             }
-            todoList.append(noItemMsg)
+            todoList.append(emptyListMsg)
         }
     })
 })
